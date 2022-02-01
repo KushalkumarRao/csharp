add975170a4240a00c65d9cfd009def6c9933807
kushalkumarrao <kushalrao1363@gmail.com>
2022-02-01 09:39:54 +0530

    firstcommit

---
+++ 24 b prgm/24 b prgm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _24_b_prgm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j =1; j <=i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
