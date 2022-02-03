01ac2c00441b4cac20a2372d10a898c3487fad89
kushalkumarrao <kushalrao1363@gmail.com>
2022-02-03 16:55:50 +0530

    firstcommit

---
+++ RealEstate/RealEstate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RealEstate.propertyListing listing = new RealEstate.propertyListing();
+            listing.CreateListing();
+            Console.Clear();
+            listing.ShowProperty();
+            System.Console.ReadKey();
+            
+
+        }
+    }
+}
